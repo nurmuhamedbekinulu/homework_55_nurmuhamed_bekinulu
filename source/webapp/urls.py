ac3fb404b715f1c92c58bc94154de4fcbcffe084
Nurmuhamed Bekinulu <nurmuhamedbekinulu@gmail.com>
2023-02-24 18:05:06 +0600

    Добавление htmlстраницы для представления update

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from webapp.views.base import index_view
-from webapp.views.tasks import add_view, detail_view
+from webapp.views.tasks import add_view, detail_view, update_view
 
 
 urlpatterns =[
     path("", index_view, name='index'),
     path("task/", index_view),
     path("task/add/", add_view, name='task_add'),
-    path("task/<int:pk>", detail_view, name='task_detail')
+    path("task/<int:pk>", detail_view, name='task_detail'),
+    path("task/<int:pk>/update/", update_view, name='task_update')
 ]
